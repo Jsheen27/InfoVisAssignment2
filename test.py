e9ee8a98c271cad61220728c53dfe18e1b81c64b
Jsheen27 <44396836+Jsheen27@users.noreply.github.com>
2021-03-20 13:11:24 +0000

    added all files

---
+++ test.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import csv
+
+df = pd.read_csv("spotify.csv")
+df.sort_values("track_id", inplace = True)
+df.drop_duplicates(subset="track_id", keep = 'first', inplace = True)
+
+df.to_csv('spotify_data.csv')
