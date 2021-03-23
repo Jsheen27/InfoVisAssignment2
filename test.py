9d9f3848df7313eeb777f358e7ee1806eecd9429
Jsheen27 <44396836+Jsheen27@users.noreply.github.com>
2021-03-23 20:06:01 +0000

    Update test.py

--- test.py
+++ test.py
@@ -6,3 +6,7 @@ df.sort_values("track_id", inplace = True)
 df.drop_duplicates(subset="track_id", keep = 'first', inplace = True)
 
 df.to_csv('spotify_data.csv')
+
+df = df[df.playlist_genre == "pop"]
+
+df.to_csv('spotify_data_pop.csv')
