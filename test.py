e9546993e8cebb6e5c4697eeb4eab66531a62c06
Jsheen27 <44396836+Jsheen27@users.noreply.github.com>
2021-03-24 11:31:15 +0000

    Update test.py

--- test.py
+++ test.py
@@ -14,3 +14,8 @@ df1.to_csv('spotify_data_pop.csv')
 df2 = df[df.playlist_genre == "edm"]
 
 df2.to_csv('spotify_data_edm.csv')
+
+df1 = df1[df1.track_popularity >47]
+df2 = df2[df2.track_popularity < 37]
+final_df = pd.concat([df1, df2]) 
+final_df.to_csv('spotify_data_pop_edm.csv', index=False)
